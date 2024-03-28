d2aed0db7c7511d56083b504e39346e513f6aa26
AlexAgruso <agrusoalexander@gmail.com>
2024-03-28 18:22:15 -0500

    configure vimtex and switch to autopairs

--- init.lua
+++ init.lua
@@ -383,7 +383,7 @@ require('lazy').setup {
       'onsails/lspkind.nvim',
     },
     config = function()
-      -- See `:help cmp`
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       local lspkind = require 'lspkind'
@@ -432,15 +432,11 @@ require('lazy').setup {
           documentation = cmp.config.window.bordered(),
         },
       }
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
     end,
   },
-
-  'folke/tokyonight.nvim',
-
-  -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  { -- Collection of various small independent plugins/modules
+  {
     'echasnovski/mini.nvim',
     config = function()
       -- Better Around/Inside textobjects
@@ -457,27 +453,9 @@ require('lazy').setup {
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      statusline.setup()
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we disable the section for
-      -- cursor information because line numbers are already enabled
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return ''
-      end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
@@ -508,7 +486,6 @@ require('lazy').setup {
       }
     end,
   },
-
   {
     'nvim-telescope/telescope-file-browser.nvim',
     dependencies = {
@@ -529,19 +506,24 @@ require('lazy').setup {
     },
   },
   {
-    'm4xshen/autoclose.nvim',
-    opts = {
-      keys = {
-        ["'"] = {
-          escape = true,
-          close = true,
-          pair = "''",
-          disabled_filetypes = {
-            'rust',
-          },
-        },
-      },
-    },
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {},
+    config = function()
+      local Rule = require 'nvim-autopairs.rule'
+      local npairs = require 'nvim-autopairs'
+
+      npairs.setup {
+        check_ts = true,
+      }
+
+      npairs.add_rules {
+        Rule('\\(', '\\)', 'tex'),
+        Rule('\\[', '\\]', 'tex'),
+        Rule('$', '$', 'tex'),
+        Rule('$$', '$$', 'tex'),
+      }
+    end,
   },
   {
     'marko-cerovac/material.nvim',
@@ -588,10 +570,12 @@ require('lazy').setup {
       }
     end,
   },
-  --  TODO: figure out latex live preview
-  -- {
-  --   'frabjous/knap',
-  -- },
+  {
+    'lervag/vimtex',
+    init = function()
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
 }
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
