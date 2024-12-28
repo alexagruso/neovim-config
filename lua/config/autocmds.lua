35bae27d37c40e287cabb3d4118a01476dff36e5
AlexAgruso <agrusoalexander@gmail.com>
2024-12-28 00:42:26 -0600

    add highlight text yank autocommand and remove autocommand groups

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,5 @@
 vim.api.nvim_create_autocmd('BufWritePre', {
-  desc = 'Run conform formatter for current buffer on save',
-  group = vim.api.nvim_create_augroup('conform', { clear = false }),
+  desc = 'Run conform.nvim formatter for current buffer on save',
   callback = function(args)
     require('conform').format { bufnr = args.buf }
   end,
@@ -8,7 +7,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 
 vim.api.nvim_create_autocmd('InsertEnter', {
   desc = 'Disable relative number lines when entering insert mode',
-  group = vim.api.nvim_create_augroup('disable-relative-number', { clear = true }),
   callback = function()
     vim.opt.relativenumber = false
   end,
@@ -16,8 +14,14 @@ vim.api.nvim_create_autocmd('InsertEnter', {
 
 vim.api.nvim_create_autocmd('InsertLeave', {
   desc = 'Enable relative number lines when exiting insert mode',
-  group = vim.api.nvim_create_augroup('enable-relative-number', { clear = true }),
   callback = function()
     vim.opt.relativenumber = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight text when copying',
+  callback = function()
+    vim.highlight.on_yank()
   end,
 })
