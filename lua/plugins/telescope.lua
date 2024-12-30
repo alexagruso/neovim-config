f3f4402d2e197d7857976fa672e614e7e580b489
AlexAgruso <agrusoalexander@gmail.com>
2024-12-30 03:35:36 -0600

    rename `vimcmd` utility function to `Vimcmd`

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,8 +4,8 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    vim.keymap.set('n', '<leader>ff', vimcmd 'Telescope find_files', { desc = '[F]ile [F]inder' })
-    vim.keymap.set('n', '<leader>fg', vimcmd 'Telescope live_grep', { desc = '[F]ile [G]rep' })
-    vim.keymap.set('n', '<leader>fh', vimcmd 'Telescope help_tags', { desc = '[H]elp tags' })
+    vim.keymap.set('n', '<leader>ff', Vimcmd 'Telescope find_files', { desc = '[F]ile [F]inder' })
+    vim.keymap.set('n', '<leader>fg', Vimcmd 'Telescope live_grep', { desc = '[F]ile [G]rep' })
+    vim.keymap.set('n', '<leader>fh', Vimcmd 'Telescope help_tags', { desc = '[H]elp tags' })
   end,
 }
--- lua/plugins/telescope_file_browser.lua
+++ lua/plugins/telescope_file_browser.lua
@@ -2,6 +2,6 @@ return {
   'nvim-telescope/telescope-file-browser.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   config = function()
-    vim.keymap.set('n', '<leader>fb', vimcmd 'Telescope file_browser', { desc = '[F]ile [B]rowser' })
+    vim.keymap.set('n', '<leader>fb', Vimcmd 'Telescope file_browser', { desc = '[F]ile [B]rowser' })
   end,
 }
