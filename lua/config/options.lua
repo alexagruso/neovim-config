e38c02a30e17ab5e230c2cc4828ba5125200742b
AlexAgruso <agrusoalexander@gmail.com>
2025-01-02 16:26:18 -0600

    add shiftwidth and tabstop options and remove format autocommand

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,6 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.showmode = false
+
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,12 @@
 return {
   'stevearc/conform.nvim',
   opts = {
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = 'fallback',
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
     },
   },
-  config = function()
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      desc = 'Run conform.nvim formatter for current buffer on save',
-      callback = function(args)
-        require('conform').format { bufnr = args.buf }
-      end,
-    })
-  end,
 }
