c0b82633b080f5dea93d49fbc5fc4b8ef13bc488
AlexAgruso <agrusoalexander@gmail.com>
2025-01-03 01:16:14 -0600

    add keymap for searching open buffers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    vim.keymap.set('n', '<leader>sb', Vimcmd 'Telescope buffers', { desc = '[S]earch [B]uffers' })
     vim.keymap.set('n', '<leader>sf', Vimcmd 'Telescope find_files', { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sg', Vimcmd 'Telescope live_grep', { desc = '[S]earch with [G]rep' })
     vim.keymap.set('n', '<leader>sh', Vimcmd 'Telescope help_tags', { desc = '[S]earch [H]elp tags' })
