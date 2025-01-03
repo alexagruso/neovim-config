2a2d245f067db32527bcae16531415b2f7df658a
AlexAgruso <agrusoalexander@gmail.com>
2025-01-03 01:14:56 -0600

    create dashboard config

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,39 @@
+return {
+  'nvimdev/dashboard-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      theme = 'hyper',
+      shortcut_type = 'number',
+      config = {
+        header = {
+          '░▒▓███████▓▒░  ░▒▓████████▓▒░  ░▒▓██████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░ ░▒▓██████████████▓▒░ ',
+          '░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░',
+          '░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░',
+          '░▒▓█▓▒░░▒▓█▓▒░ ░▒▓██████▓▒░   ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░',
+          '░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▓█▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░',
+          '░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▓█▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░',
+          '░▒▓█▓▒░░▒▓█▓▒░ ░▒▓████████▓▒░  ░▒▓██████▓▒░     ░▒▓██▓▒░    ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░',
+        },
+        -- leading space required for labels due to shitty plugin
+        project = {
+          enable = true,
+          limit = 10,
+          icon = '',
+          label = ' Recent Projects',
+          action = 'Telescope find_files cwd=',
+        },
+        mru = {
+          enable = true,
+          limit = 10,
+          icon = '',
+          label = ' Recent Files',
+          cwd_only = false,
+        },
+        shortcut = {}, -- removes default shortcuts
+        footer = {},
+      },
+    }
+  end,
+}
