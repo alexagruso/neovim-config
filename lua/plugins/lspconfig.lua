9d14c19bab00c17ab5206a3b6a50468e0e918ceb
alex <agrusoalexander@gmail.com>
2025-01-07 23:02:44 -0600

    add json language server

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -32,6 +32,7 @@ return {
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     local language_servers = {
+      jsonls = {},
       lua_ls = {},
       rust_analyzer = {},
       svelte = {},
