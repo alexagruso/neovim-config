b0164411a55c22c63bc5eb803e689709080e80d6
alex <agrusoalexander@gmail.com>
2025-02-28 17:36:15 -0600

    add formaters and lsps to ensure installed, change capitalization in which-key hints

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -21,17 +21,23 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        lsp_keymap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        lsp_keymap('grf', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        lsp_keymap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        lsp_keymap('<leader>rn', vim.lsp.buf.rename, '[R]e[N]ame')
+        lsp_keymap('gd', require('telescope.builtin').lsp_definitions, '[g]oto [d]efinition')
+        lsp_keymap('gD', vim.diagnostic.open_float, '[g]oto [D]iagnostic')
+        lsp_keymap('grf', require('telescope.builtin').lsp_references, '[g]oto [r]e[f]erences')
+        lsp_keymap('gI', require('telescope.builtin').lsp_implementations, '[g]oto [I]mplementation')
+        lsp_keymap('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
       end,
     })
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
+    -- List of supported language servers
     local language_servers = {
+      bashls = {},
+      cssls = {},
+      html = {},
+      intelephense = {},
       jsonls = {},
       lua_ls = {},
       pylsp = {},
@@ -54,10 +60,16 @@ return {
     }
 
     local formatters = {
+      'beautysh',
       'black',
+      'clang-format',
+      'fixjson',
+      'gofumpt',
       'latexindent',
       'prettierd',
+      'pretty-php',
       'stylua',
+      'xmlformatter',
     }
 
     require('mason-tool-installer').setup {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,10 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    vim.keymap.set('n', '<leader>sb', Vimcmd 'Telescope buffers', { desc = '[S]earch [B]uffers' })
-    vim.keymap.set('n', '<leader>sf', Vimcmd 'Telescope find_files', { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sg', Vimcmd 'Telescope live_grep', { desc = '[S]earch with [G]rep' })
-    vim.keymap.set('n', '<leader>sh', Vimcmd 'Telescope help_tags', { desc = '[S]earch [H]elp tags' })
+    vim.keymap.set('n', '<leader>sb', Vimcmd 'Telescope buffers', { desc = '[s]earch [b]uffers' })
+    vim.keymap.set('n', '<leader>sd', Vimcmd 'Telescope diagnostics', { desc = '[s]earch [d]iagnostics' })
+    vim.keymap.set('n', '<leader>sf', Vimcmd 'Telescope find_files', { desc = '[s]earch [f]iles' })
+    vim.keymap.set('n', '<leader>sg', Vimcmd 'Telescope live_grep', { desc = '[s]earch with [g]rep' })
+    vim.keymap.set('n', '<leader>sh', Vimcmd 'Telescope help_tags', { desc = '[s]earch [h]elp tags' })
   end,
 }
