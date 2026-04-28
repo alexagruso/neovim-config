07d9280a617f8ed81b269009c38eddc4f1c3294a
alex <agrusoalexander@gmail.com>
2026-04-28 14:29:58 -0500

    fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,25 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  opts = {
-    ensure_installed = {
-      'css',
-      'html',
-      'javascript',
-      'latex',
-      'lua',
-      'scss',
-      'svelte',
-      'typescript',
-    },
-    auto_install = true,
-
-    highlight = {
-      enable = true,
-      use_languagetree = true,
-    },
-
-    indent = { enable = true },
-  },
+  config = function()
+    local configs = require 'nvim-treesitter.configs'
+    configs.setup {
+      ensure_installed = {
+        'css',
+        'html',
+        'javascript',
+        'latex',
+        'lua',
+        'scss',
+        'svelte',
+        'typescript',
+        'wgsl',
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+    }
+  end,
 }
